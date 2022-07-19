d148057e7be24df3a87fb9a5f435b94c5a67b43c
WilsonTechPro <wilsonfamily09112@gmail.com>
2022-07-19 16:31:20 -0500

    add exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,7 +14,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var array
      */
     protected $dontReport = [
         //
@@ -19,7 +23,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $dontFlash = [
         'current_password',
@@ -37,5 +41,44 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        $code = $exception->getCode();
+        $message = $exception->getMessage();
+        if ($code < 100 || $code >= 600) {
+            $code = \Illuminate\Http\Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            $message = $exception->getMessage();
+            $code = \Illuminate\Http\Response::HTTP_NOT_FOUND;
+
+            if (preg_match('@\\\\(\w+)\]@', $message, $matches)) {
+                $model = $matches[1];
+                $model = preg_replace('/Table/i', '', $model);
+                $message = "{$model} not found.";
+            }
+        }
+
+        if ($exception instanceof ValidationException) {
+            $validator = $exception->validator;
+            $message = $validator->errors()->first();
+            $code = \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY;
+
+            if (! $request->expectsJson() and ! $request->isXmlHttpRequest()) {
+                return Redirect::back()->withInput()->withErrors($message);
+            }
+        }
+
+        if ($request->expectsJson() or $request->isXmlHttpRequest()) {
+            return Response::json([
+                'success' => false,
+                'message' => $message,
+            ], $code);
+        }
+
+        return parent::render($request, $exception);
     }
 }
